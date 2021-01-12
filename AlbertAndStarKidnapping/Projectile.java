79d39fb6775bd3be6c8fd30048a43e343d8b1d3c
lz2003 <lucy2003.zhao@gmail.com>
2021-01-12 16:10:11 -0500

    added projectiles

--- AlbertAndStarKidnapping/Projectile.java
+++ AlbertAndStarKidnapping/Projectile.java
@@ -1,30 +1,95 @@
 import greenfoot.*;
-
+import java.util.ArrayList;
 /**
- * Write a description of class Projectile here.
+ * A projectile that hits enemies
  * 
- * @author (your name) 
+ * @author Lucy Zhao
  * @version (a version number or a date)
  */
-public class Projectile extends Sprite 
+public abstract class Projectile extends Sprite 
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    public Projectile(double x, double y, GreenfootImage image) {
-        super(x, y, image, 100, 100, 1);
-        setLocation(x, y);
+    // Max level of the projectile
+    protected static final int MAX_LEVEL = 3;
+    
+    protected float damage;
+    protected float speed;
+    protected Enemy target;
+    
+    /**
+     * Constructor for Projectile class
+     * 
+     * @param x         the starting x coordinate
+     * @param y         the starting y coordinate
+     * @param image     the sprite of the projectile
+     * @param target    the reference enemy target
+     */
+    public Projectile(double x, double y, GreenfootImage image, Enemy target) {
+        super(x, y, image, image.getWidth(), image.getHeight(), 1);
+        setLocation(x, y); 
+        turnTowards(target.getX(), target.getY());
+        Global.getManager().addProjectile(this);
     }
     
     /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * Update method
      */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public void _update(float delta) {
+        if (isRemoved()) return;
+        move(speed);
+        // Maybe don't run it each loop?
+        checkCollision();
+        checkWorldBounds();
+    }
+    
+    /**
+     * Checks if projectile has reached an enemy
+     */
+    protected void checkCollision() {
+        ArrayList<Enemy> enemies = Global.getManager().getEnemies();
+        target = null;
+        // Right now, the enemy has to be within half of the projectile's height
+        // Can be changed if needed
+        double smallestDist = getHeight()/2;
+        for (Enemy e : enemies) {
+            double dist = Math2D.distance(getX(), e.getX(), getY(), e.getY());
+            if (dist <= smallestDist) {
+                smallestDist = dist;
+                target = e;
+            }
+        }
+        damageEnemy();
+    }
+    
+    /**
+     * Check if the projectile has reached the edge of the world
+     * 
+     * Might have to change this if we have a moving camera.
+     */
+    protected void checkWorldBounds() {
+        if (getX() < 0 || getX() > Global.getWorld().getWidth()) {
+            destroy();
+        } else if (getY() < 0 || getY() > Global.getWorld().getHeight()) {
+            destroy();
+        }
+    }
+    
+    /**
+     * Damages the enemy
+     */
+    protected void damageEnemy() {
+        if (target != null && !isRemoved()) {
+            //target.damage(damage);
+            //System.out.println("HIT");
+            destroy();
+        }
+    }
+    
+    /**
+     * Destroys the projectile
+     */
+    protected void destroy() {
+        if (!isRemoved()) {
+            removeSprite();
+        }
     }
 }
--- AlbertAndStarKidnapping/Splash.java
+++ AlbertAndStarKidnapping/Splash.java
@@ -1,30 +1,86 @@
 import greenfoot.*;
-
+import java.util.ArrayList;
 /**
- * Write a description of class Splash here.
+ * Projectiles that can deal area of effect damage
  * 
- * @author (your name) 
+ * @author Lucy Zhao
  * @version (a version number or a date)
  */
 public class Splash extends Projectile 
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    public Splash(double x, double y, GreenfootImage image) {
-        super(x, y, image);
-        setLocation(x, y);
-    }
-
+    // Should be changed later on
+    private double radius = 100;
+    private ArrayList<Enemy> targets;
+    // Animated explosion animation when it hits ?
+    private Animation onHit; 
+    // Potentially a instance variable for effects?
+    
     /**
-     * An example of a method - replace this comment with your own
+     * Constructor for Splash class
      * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * @param x         the starting x coordinate
+     * @param y         the starting y coordinate
+     * @param image     the sprite of the projectile
+     * @param target    the reference enemy target
      */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public Splash(double x, double y, GreenfootImage image, Enemy target) {
+        super(x, y, image, target);
+    }
+    
+    /**
+     * Constructor for Splash class
+     * 
+     * @param x         the starting x coordinate
+     * @param y         the starting y coordinate
+     * @param image     the sprite of the projectile
+     * @param target    the reference enemy target
+     * @param onHit     animation when splash hits an enemy
+     */
+    public Splash(double x, double y, GreenfootImage image, Enemy target, Animation onHit) {
+        super(x, y, image, target);
+        // Intialize animation
+    }
+    
+    /**
+     * Overridden from the Projectile superclass. Finds multiple enemies
+     */
+    protected void checkCollision() {
+        ArrayList<Enemy> enemies = Global.getManager().getEnemies();
+        targets = new ArrayList<Enemy>();
+        target = null;
+        double smallestDist = getHeight()/2;
+        for (Enemy e : enemies) {
+            double dist = Math2D.distance(getX(), e.getX(), getY(), e.getY());
+            // Find the enemy who is directly hit by the splash
+            if (dist <= smallestDist) {
+                smallestDist = dist;
+                target = e;
+                targets.add(e);
+            } // Find enemies that are within the radius
+            else if (dist <= radius) {
+                targets.add(e);
+            }
+        }
+        damageEnemy();
+    }
+    
+    /**
+     * Overridden from the Projectile superclass. Hurts multiple enemies
+     */
+    protected void damageEnemy() {
+        // If the projectile directly hits an enemy, then hurt everyone in the radius
+        if (target != null && !isRemoved()) {
+            for (Enemy e: targets) {
+                //e.damage(damage);
+                //System.out.println("HIT");
+            }
+            destroy();
+        }
+    }
+    
+    /**
+     * Play an animation when the splash projectile hits a target
+     */
+    protected void explode() {
     }
 }
