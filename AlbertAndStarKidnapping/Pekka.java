40123188bde8cea6c3f63f3cca9be5f0c12801d4
pokedeck27 <rachelvanessatong@gmail.com>
2021-01-14 22:05:54 -0500

    cleaned up code and did some commenting

--- AlbertAndStarKidnapping/Pekka.java
+++ AlbertAndStarKidnapping/Pekka.java
@@ -1,32 +1,23 @@
 import greenfoot.*;
 
 /**
- * Write a description of class Pekka here.
+ * The Pekka is a large unit that has a large amount of health, large damage, and very slow. 
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
 public class Pekka extends Enemy 
 {
-    // instance variables - replace the example below with your own
-    private int x;
 
     /**
-     * Constructor for objects of class Pekka
+     * Constructor for Pekka
+     * 
+     * @param x         the x coordinate of Pekka
+     * @param y         the y coordinate of Pekka
+     * @param image     image representing the Pekka
      */
     public Pekka(double x, double y, GreenfootImage image) {
         super(x, y, image, 500, 50, 3, 2f, false, false);
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
