969b2b46fbbb8f964dbc496a96e576eb41438069
pokedeck27 <rachelvanessatong@gmail.com>
2021-01-26 17:17:37 -0500

    edited spelling/grammar/format of cutscene + scene8 + images

--- AlbertAndStarKidnapping/Cutscene.java
+++ AlbertAndStarKidnapping/Cutscene.java
@@ -13,18 +13,20 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Cutscene extends World
 {
     public static final int LEVELS_PER_CUTSCENE = 5;
-    public static final int TOTAL_CUTSCENES = 7;
+    public static final int TOTAL_CUTSCENES = 8;
     //Background images
     private static GreenfootImage lightCastle = new GreenfootImage("images/cutscene/lc.jpeg");
     private static GreenfootImage insideCastle = new GreenfootImage("images/cutscene/ic.jpeg");
     private static GreenfootImage darkCastle = new GreenfootImage("images/cutscene/dc.jpeg");
+    private static GreenfootImage warGround = new GreenfootImage("images/cutscene/wb.jpeg");
     
     //Character images
     private static GreenfootImage player = new GreenfootImage("images/cutscene/bennet.png");
     private static GreenfootImage iroh = new GreenfootImage("images/cutscene/albedo.png");
     private static GreenfootImage koyen = new GreenfootImage("images/cutscene/childe.png");
-    private static GreenfootImage jayjay = new GreenfootImage("images/cutscene/kaeya.png");
+    private static GreenfootImage jayjay = new GreenfootImage("images/cutscene/dvalin2.png");
     private static GreenfootImage council = new GreenfootImage("images/cutscene/sucrose.png");
+    private static GreenfootImage yajyaj = new GreenfootImage("images/cutscene/kaeya.png");
     
     //Current scene being displayed
     public int sceneNum;
@@ -37,6 +39,7 @@ public class Cutscene extends World
     private Textbox textBox5;
     private Textbox textBox6;
     private Textbox textBox7;
+    private Textbox textBox8;
     /**
      * Constructor for world. Creates an AbilityButton to demonstrate how it works.
      * 
@@ -54,8 +57,8 @@ public class Cutscene extends World
         textBox4 = new Textbox("Let me just cut to the chase", "Courier New", 20, 500, 100, 6, Color.WHITE, Color.BLACK, koyen);
         textBox5 = new Textbox("It’s been 3 months already, why\nhaven’t the council crowned me King.", "Courier New", 20, 500, 100, 6, Color.WHITE, Color.BLACK, koyen);
         textBox6 = new Textbox("*5 Years Ago*", "Courier New", 20, 500, 100, 6, Color.WHITE, Color.BLACK, council);
-        textBox7 = new Textbox("Its time to finally get revenge\nfor my father. We pushed the war back\nto your turf.", "Courier New", 20, 500, 100, 6, Color.WHITE, Color.BLACK, player);
-        
+        textBox7 = new Textbox("*Present Time", "Courier New", 20, 500, 100, 6, Color.WHITE, Color.BLACK, player);
+        textBox8 = new Textbox("*100 Years Later*", "Courier New", 20, 500, 100, 6, Color.WHITE, Color.BLACK, player);
         //Variable to display chosen scene
         this.sceneNum = sceneNum;
         switch(sceneNum)
@@ -81,6 +84,9 @@ public class Cutscene extends World
                 case 7:
                     scene7();
                     break;
+                 case 8:
+                    scene8();
+                    break;
         }
         //Ensures that the textbox is in front of the background and character image. 
         setPaintOrder(Textbox.class);
@@ -103,6 +109,7 @@ public class Cutscene extends World
             textBox5.updateText();
             textBox6.updateText();
             textBox7.updateText();
+            textBox8.updateText();
         }
     }
     
@@ -110,8 +117,9 @@ public class Cutscene extends World
     {
         setBackground(darkCastle);
         addObject(textBox, 425, 600);
-        textBox.addText("If I am going to get back what truly belongs to \nme, I, the great Firelord and Pope of the church \nof Greenfoot, the mighty Koyen Zordan Klean,", koyen);
-        textBox.addText("better act now. After all these years I have \nfinally built a kingdom and a strong enough\narmy to take him down.", koyen);
+        textBox.addText("If I'm going to get back what truly\nbelongs to me,", koyen);
+        textBox.addText("I, the great Firelord and Pope of the\nchurch of Greenfoot, the mighty Koyen\nZordan Klean, better act now.", koyen);
+        textBox.addText("After all these years I have finally\nbuilt a kingdom and a strong enough\narmy to take him down.", koyen);
         textBox.addText("Harnessing the power from the God of\nGreenfoot, the only thing in my way is\nthat dragon of his.", koyen);
         textBox.addText("It’s already getting too strong for my\nliking. General, it's time to prepare\nfor war! Huehuehuehue!", koyen);
     }
@@ -123,7 +131,7 @@ public class Cutscene extends World
         textBox2.addText("He must be behind all this fighting.", player);
         textBox2.addText("I may be young, but I have prepared\nfor this moment ever since I became\nking.", player);
         textBox2.addText("I’ll never forget the pain he caused\nme.", player);
-        textBox2.addText("You’ve raised me well, I’m shall \ndefend this kingdom with my life.\n Jay jay jaaaaaaaay!", jayjay);
+        textBox2.addText("You’ve raised me well, I shall\ndefend this kingdom with my life.\nJay jay jaaaaaaaay!", jayjay);
     }
     
     public void scene3()
@@ -133,8 +141,8 @@ public class Cutscene extends World
         textBox3.addText("What’s up step bro? It isn’t often that\nyou visit me.", iroh);
         textBox3.addText("Take some advice and speak more\nformally. You are the king after all,\nbut not for long though.", koyen);
         textBox3.addText("What does that mean? Is there\nsomething else you prefer I call you?\nHow about Happyfeet instead.", iroh);
-        textBox3.addText("I’ve been thinking. The Fireload and \nPope of Greenfoot has much more\npower than the king.", koyen);
-        textBox3.addText("People are more willing to follow religion \nthan some temporary king. Wouldn’t it\nbe easier if I was both.", koyen);
+        textBox3.addText("I’ve been thinking. The Firelord and\nPope of Greenfoot has much more\npower than the king.", koyen);
+        textBox3.addText("People are more willing to follow\nreligion than some temporary king.\nWouldn’t it be easier if I was both?", koyen);
     }
     
     public void scene4()
@@ -155,7 +163,7 @@ public class Cutscene extends World
         addObject(textBox5, 425, 600);
         textBox5.addText("Following the death of King Iroh,\nhis son soon became the successor\nand inherited the throne.", council);
         textBox5.addText("What! He doesn’t have a son", koyen);
-        textBox5.addText("His highness was going to be\nintroduced him to you the day you visited.", council);
+        textBox5.addText("His highness was going to be\nintroduced him to you the day you\nvisited....", council);
         textBox5.addText("Before he could, he witnessed\nyou kill his father.", council);
         textBox5.addText("*Koyen escapes*", koyen);
     }
@@ -175,11 +183,27 @@ public class Cutscene extends World
     {
         setBackground(darkCastle);
         addObject(textBox7, 425, 600);
+        textBox7.addText("Its time to finally get revenge\nfor my father. We pushed the war back\nto your turf.", player);
         textBox7.addText("It doesn’t matter, I’ve got the\npower of God and anime on my side!", koyen);
         textBox7.addText("We have THE Jay Jay", player);
         textBox7.addText("Everything changed when the\nGreenfoot nation attacked.", player);
         textBox7.addText("When the world needed him the most,\nhe vanished", player);
         textBox7.addText("But I believe Jay Jay can save\nthe world", player);
         textBox7.addText("Uwu yes daddy. Jay Jay!", jayjay);
+    }
+    
+    public void scene8()
+    {
+        setBackground(warGround);
+        addObject(textBox8, 425, 600);
+        textBox8.addText("This war has lasted 100 years. What\n happened? I thought we were winning", player);
+        textBox8.addText("Good thing we don't age. It's\nbecause of that new general YajYaj", jayjay);
+        textBox8.addText("It's true he's great. I found\nhim in some ditch and now its fair", koyen);
+        textBox8.addText("We must find a way to make amends\nthis war will last forever", player);
+        textBox8.addText("The name is yajyaj, you know my\nstrategy is just making the troops walk\nin a line right?", yajyaj);
+        textBox8.addText("That man has 10000 iq", jayjay);
+        textBox8.addText("I'm willing to fight to the end\nof the world", koyen);
+        textBox8.addText("Then I guess we are too", player);
+        textBox8.addText("*End of story*\n*There will now be infinite levels*", player);
     }
 }
